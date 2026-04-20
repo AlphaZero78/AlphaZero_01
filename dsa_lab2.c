7ba8acc75dc366f5c3adb22cad1a409f822f2e6d
AlphaZero <3125874730@qq.com>
2026-04-20 15:18:18 +0800

    lab2.1

--- dsa_lab2.c
+++ dsa_lab2.c
@@ -87,17 +87,19 @@ int main(){
             }
         }
         if(IsEmpty(&Emergency_queue)) {
-            if(!IsEmpty(&Normal_queue)) { 
-                for(int i=0;i<3;i++) {
-                    if(!doctors[i].isBusy) {
-                        Patient *p = Dequeue(&Normal_queue);
-                        doctors[i].currentP = *p;
-                        doctors[i].isBusy = true;
-                        doctors[i].finishTime = time + p->duration;
-                        fprintf(fp, "[Time %d]: %d号医生接诊患者 %s ,预计 Time %d 结束\n", time, doctors[i].id, p->name, doctors[i].finishTime);
-                        break;
+            for(int i=0;i<3;i++) {
+                if(!IsEmpty(&Normal_queue)) { 
+                    for(int i=0;i<3;i++) {
+                        if(!doctors[i].isBusy) {
+                            Patient *p = Dequeue(&Normal_queue);
+                            doctors[i].currentP = *p;
+                            doctors[i].isBusy = true;
+                            doctors[i].finishTime = time + p->duration;
+                            fprintf(fp, "[Time %d]: %d号医生接诊患者 %s ,预计 Time %d 结束\n", time, doctors[i].id, p->name, doctors[i].finishTime);
+                            break;
+                        }
                     }
-                }
+                }   
             }         
         }
         else{
